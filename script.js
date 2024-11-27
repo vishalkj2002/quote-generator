9700ef91ecc1f1372a8756b94b3bdb5f56b88bc8
Vishal Kumar Jangid <imrvishal28@gmail.com>
2024-11-27 15:07:47 +0530

    feat: add favicon, enhance quote functionality, and improve responsive design

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const quoteText = document.getElementsByClassName("quote")[0];
 const authorName = document.getElementsByClassName("name")[0];
+const quoteButton = document.getElementsByClassName("new-quote")[0];
+const soundButton = document.getElementsByClassName("sound")[0];
+const copyButton = document.getElementsByClassName("copy")[0];
+const xButton = document.getElementsByClassName("x")[0];
 
 async function randomQuote() {
+  quoteButton.classList.add("loading");
+  quoteButton.textContent = "Loading Quote...";
   const apiKey = "vP+wvIBT0T5ujNMw18zDFA==IMb1klh30vZFKilQ";
   const response = await fetch("https://api.api-ninjas.com/v1/quotes", {
     headers: {
@@ -13,7 +19,25 @@ async function randomQuote() {
     const data = await response.json();
     quoteText.textContent = data[0].quote;
     authorName.textContent = data[0].author;
+    quoteButton.textContent = "New Quote";
+    quoteButton.classList.remove("loading");
   } else {
     quoteText.textContent = "Failed to fetch quote";
   }
 }
+
+soundButton.addEventListener("click", () => {
+  let utterance = new SpeechSynthesisUtterance(
+    `${quoteText.textContent} by ${authorName.textContent}`
+  );
+  speechSynthesis.speak(utterance);
+});
+
+copyButton.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.textContent);
+});
+
+xButton.addEventListener("click", () => {
+  let tweetUrl = `https://x.com/intent/tweet?url=${quoteText.textContent}`;
+  window.open(tweetUrl, "_blank");
+});
